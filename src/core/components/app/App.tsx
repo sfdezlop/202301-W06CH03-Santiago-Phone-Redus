eaa93ba107887119252ac165f066d8d3494e2191
sfernandez <sfdezlop@gmail.com>
2023-02-22 19:53:23 +0100

    Change App component

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">202301-W06CH03-Santiago-Phone-Redus</div>;
+  return (
+    <div className="App">Challenge 202301-W06CH03-Santiago-Phone-Redus</div>
+  );
 }
 
 export default App;
