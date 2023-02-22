74120ae8dbf0038675ac197c60c3ea849afd6f65
sfernandez <sfdezlop@gmail.com>
2023-02-22 19:50:39 +0100

    Update app eliminate counter

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from '../../../features/counter/Counter';
 
 function App() {
   return <div className="App">202301-W06CH03-Santiago-Phone-Redus</div>;
