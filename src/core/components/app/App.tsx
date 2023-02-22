7659a0fa4c1df36b30781b5e4649d39a6328f7d1
sfernandez <sfdezlop@gmail.com>
2023-02-22 19:26:12 +0100

    Change app component

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Counter } from "../../../features/counter/Counter";
-import "./App.css";
+import React from 'react';
+import { Counter } from '../../../features/counter/Counter';
 
 function App() {
   return <div className="App">202301-W06CH03-Santiago-Phone-Redus</div>;
