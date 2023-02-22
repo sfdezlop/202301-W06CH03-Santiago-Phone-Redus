fdc6af662e42421442f711eb76664cfe56b58310
sfernandez <sfdezlop@gmail.com>
2023-02-22 18:54:25 +0100

    Clean folder structure

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Counter } from "../../../features/counter/Counter";
+import "./App.css";
+
+function App() {
+  return <div className="App">202301-W06CH03-Santiago-Phone-Redus</div>;
+}
+
+export default App;
