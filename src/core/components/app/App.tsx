f13dd1042703ab71183a52a53a233b4093b99571
sfernandez <sfdezlop@gmail.com>
2023-02-23 06:38:57 +0100

    Change app component

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,13 +1,14 @@
 import { Keyboard } from '../keyboard/keyboard';
 import { Display } from '../display/display';
+import { Actions } from '../actions/actions';
 
 function App() {
   return (
-    // <div className="App">Challenge 202301-W06CH03-Santiago-Phone-Redus</div>
     <div className="container">
       <Display></Display>
       <main className="phone">
         <Keyboard></Keyboard>
+        <Actions></Actions>
       </main>
     </div>
   );
