b09fab513a3423c7965488da684e7e646d0425ac
sfernandez <sfdezlop@gmail.com>
2023-02-23 05:46:39 +0100

    Update app and test

--- src/core/components/app/App.test.tsx
+++ src/core/components/app/App.test.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "../../store/store";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../store/store';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/phone/i)).toBeInTheDocument();
+  expect(getByText(/1/i)).toBeInTheDocument();
 });
--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { Keyboard } from '../keyboard/keyboard';
+
 function App() {
   return (
-    <div className="App">Challenge 202301-W06CH03-Santiago-Phone-Redus</div>
+    // <div className="App">Challenge 202301-W06CH03-Santiago-Phone-Redus</div>
+    <div className="container">
+      <main className="phone">
+        <Keyboard></Keyboard>
+      </main>
+    </div>
   );
 }
 
